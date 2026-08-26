59db3eca6cf98a7d3386b98f97bf0a42f860765e
ahrong0820 <ahrong0820@gmail.com>
2026-08-26 12:56:40 +0900

    style: load history record action layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './calendar-layout.css'
+import './history-record-actions.css'
 
 createRoot(document.getElementById('root')!).render(<App />)
