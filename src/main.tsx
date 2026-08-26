a0f99898d4cc4d175c4da4b215b61dc441899c3e
ahrong0820 <ahrong0820@gmail.com>
2026-08-26 14:34:51 +0900

    style: load cube disposal styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,6 @@ import './history-record-actions.css'
 import './timeline-summary.css'
 import './ui-polish.css'
 import './ingredient-model.css'
+import './disposal.css'
 
 createRoot(document.getElementById('root')!).render(<App />)
